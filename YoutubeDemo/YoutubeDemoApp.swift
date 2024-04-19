aa420879155bee044b3222972a32a07a74b2f1db
Preet Karia <karia.preet5@gmail.com>
2024-04-19 01:15:54 -0400

    Initial Commit

---
+++ YoutubeDemo/YoutubeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeDemoApp.swift
+//  YoutubeDemo
+//
+//  Created by Preet Karia on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
